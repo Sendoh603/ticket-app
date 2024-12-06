ec3324f51b255b818804f90e6d116b7927d364a9
Wei Zhang <sheepchina@hotmail.com>
2024-12-06 15:29:03 -0500

    Add a link

--- app/{component}/Nav.jsx
+++ app/{component}/Nav.jsx
@@ -1,6 +1,20 @@
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
+
 const Nav = () => {
     return (
-        <div>Nav</div>
+        <nav>
+            <div>
+                <Link href="/">
+                    <FontAwesomeIcon icon={faHome} className="icon" />
+                </Link>
+                <Link href="/TicketPage/new">
+                    <FontAwesomeIcon icon={faTicket} className="icon" />
+                </Link>
+            </div>
+            <div>
+                <p className="text-default-text">Wei@gmail.com</p>
+            </div>
+        </nav>
     )
 }
 
